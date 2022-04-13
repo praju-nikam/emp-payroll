b22e9940afd372fc2501f96670181ee8692bcb9b
praju <sawantprajakta84@gmail.com>
2022-04-13 20:34:07 +0530

    Added Employee PayRoll System

---
+++ src/main/java/com/bridgelabz/EmployeePayRollFileService.java
@@ -0,0 +1,80 @@
+package com.bridgelabz;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class EmployeePayRollFileService
+{
+    public String DATA_FILE = "PayRollData.txt";
+
+    EmployeePayRollFileService() {
+
+    }
+
+    public void writeData(List<EmployeePayRoll> empPayRollList) {
+        StringBuffer data = new StringBuffer();
+        for (EmployeePayRoll e : empPayRollList) {
+            data.append(e.toString() + "\n");
+        }
+        Path filePath = Paths.get(DATA_FILE);
+        try {
+            Files.write(filePath, data.toString().getBytes());
+        } catch (IOException e1) {
+            e1.printStackTrace();
+        }
+    }
+
+    public int noOfEntries() {
+        int count = 0;
+        Path filePath = Paths.get(DATA_FILE);
+        try {
+            count = (int) Files.lines(filePath).count();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return count;
+    }
+
+    public void printData() {
+        Path filePath = Paths.get(DATA_FILE);
+        try {
+            Stream<String> stringStream = Files.lines(filePath);
+            stringStream.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public List<EmployeePayRoll> readData() {
+        List<String[]> listOfElements = null;
+        List<EmployeePayRoll> empPayRoll = new ArrayList<EmployeePayRoll>();
+        Path filePath = Paths.get(DATA_FILE);
+        String[] data = new String[3];
+        int i = 0;
+        try {
+            Stream<String> stringStr = Files.lines(filePath);
+            listOfElements = stringStr.map(s -> s.split(", ")).collect(Collectors.toList());
+            for (String[] e : listOfElements) {
+                for (String s : e) {
+                    data[i] = e[i].split("=")[1];
+                    i += 1;
+                }
+                i = 0;
+                empPayRoll.add(new EmployeePayRoll(Integer.parseInt(data[0]), data[1], Double.parseDouble(data[2])));
+            }
+            return empPayRoll;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+}
---
+++ src/main/java/com/bridgelabz/EmployeePayRollService.java
@@ -0,0 +1,77 @@
+package com.bridgelabz;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class EmployeePayRollService
+{
+    private static List<EmployeePayRoll> empPayRollList;
+    private static Scanner scanner = new Scanner(System.in);
+
+    public EmployeePayRollService(List<EmployeePayRoll> empPayRollList) {
+        this.empPayRollList = empPayRollList;
+    }
+
+    public EmployeePayRollService() {
+
+    }
+
+    public static void main(String[] args) {
+        ArrayList<EmployeePayRoll> empPayRollList = new ArrayList<EmployeePayRoll>();
+        EmployeePayRollService empPayRollService = new EmployeePayRollService(empPayRollList);
+
+        empPayRollService.readData();
+
+        empPayRollService.writeData("Console");
+
+    }
+
+    public static void readData() {
+        System.out.println("Please Enter the following details :");
+        System.out.println("Enter ID");
+        int id = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter Name :");
+        String name = scanner.nextLine();
+        System.out.println("Enter Salary :");
+        double salary = scanner.nextDouble();
+
+        EmployeePayRoll empPayRollObject = new EmployeePayRoll(id, name, salary);
+        empPayRollList.add(empPayRollObject);
+    }
+
+    public int readData(String source) {
+        List<EmployeePayRoll> empPayRoll = new ArrayList<EmployeePayRoll>();
+        if (source.equals("File")) {
+            empPayRoll = new EmployeePayRollFileService().readData();
+            return empPayRoll.size();
+        }
+        return 0;
+    }
+
+    public void writeData(String destination) {
+        if (destination.equals("Console"))
+            System.out.println("Employee Pay Roll Data : \n" + empPayRollList.get(0).toString());
+        else if (destination.equals("File"))
+            new EmployeePayRollFileService().writeData(empPayRollList);
+    }
+
+    public int noOfEntries(String destination) {
+        if (destination.equals("Console"))
+            return empPayRollList.size();
+        else if (destination.equals("File"))
+            return new EmployeePayRollFileService().noOfEntries();
+        return 0;
+    }
+
+    public void printData(String destination) {
+        if (destination.equals("Console"))
+            for (EmployeePayRoll e : empPayRollList) {
+                System.out.println(e.toString() + "\n");
+            }
+        else if (destination.equals("File"))
+            new EmployeePayRollFileService().printData();
+
+    }
+}
